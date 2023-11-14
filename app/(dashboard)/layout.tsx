027e6c162cca2fecdf71f6a1ff9f0cc0fc4ac500
Devi prasad <prasad20697@gmail.com>
2023-11-14 15:22:42 +0530

    dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,15 @@
+import ThemeSwitcher from "@/components/theme-switcher";
 import { UserButton } from "@clerk/nextjs";
 import React, { ReactNode } from "react";
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
 	return (
 		<div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
-			<nav>
-				<UserButton afterSignOutUrl="/sign-in" />
+			<nav className="flex justify-between border-b border-border h-16 px-4 py-2 items-center">
+				<div className="flex items-center gap-4">
+					<ThemeSwitcher />
+					<UserButton afterSignOutUrl="/sign-in" />
+				</div>
 			</nav>
 
 			<main className="flex w-full flex-grow">{children}</main>
