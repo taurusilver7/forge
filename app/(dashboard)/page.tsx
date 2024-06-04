aa0aede49a25d1d43cf1b95679f6b8f8846717d9
Devi prasad <prasad20697@gmail.com>
2024-06-04 08:18:15 +0530

    dashboard page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,109 @@
-import React from 'react'
+import { GetFormStats } from "@/actions/form";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import {
+	EyeOpenIcon,
+	CursorArrowIcon,
+	ViewVerticalIcon,
+	MixerVerticalIcon,
+} from "@radix-ui/react-icons";
+import React, { ReactNode, Suspense } from "react";
 
-const DashboardPage = () => {
-  return (
-    <div className='container pt-4'>Dashboard Page</div>
-  )
+interface StatCardsProps {
+	data?: Awaited<ReturnType<typeof GetFormStats>>;
+	loading: boolean;
 }
 
-export default DashboardPage
+const DashboardPage = () => {
+	return (
+		<div className="container pt-4">
+			<Suspense fallback={<StatCards loading={true} />}>
+				<CardStatsWrapper />
+			</Suspense>
+		</div>
+	);
+};
 
-function CardStatsWrapper() {}
+export default DashboardPage;
+
+async function CardStatsWrapper() {
+	const stats = await GetFormStats();
+
+	return <StatCards loading={false} data={stats} />;
+}
+
+function StatCards(props: StatCardsProps) {
+	const { data, loading } = props;
+
+	return (
+		<div className="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+			<StatsCard
+				title="Total Visits"
+				icon={<EyeOpenIcon className="text-blue-600 h-6 w-6" />}
+				helperText="All time vists"
+				value={data?.visits.toLocaleString() || ""}
+				loading={loading}
+				className="shadow-md shadow-blue-600"
+			/>
+			<StatsCard
+				title="Total Submissions"
+				icon={<ViewVerticalIcon className="text-yellow-600 w-6 h-6" />}
+				helperText="All time submissions."
+				value={data?.submissions.toLocaleString() || ""}
+				loading={loading}
+				className="shadow-md shadow-yellow-600"
+			/>
+			<StatsCard
+				title="Submissions rate"
+				icon={<CursorArrowIcon className="text-green-600 w-6 h-6" />}
+				helperText="visits that submitted form."
+				value={data?.visits.toLocaleString() || ""}
+				loading={loading}
+				className="shadow-md shadow-green-600"
+			/>
+			<StatsCard
+				title="Bounce rate"
+				icon={<MixerVerticalIcon className="text-rose-600 h-6 w-6" />}
+				helperText="Visits that leave without interacting."
+				value={data?.visits.toLocaleString() || ""}
+				loading={loading}
+				className="shadow-md shadow-rose-600"
+			/>
+		</div>
+	);
+}
+
+const StatsCard = ({
+	title,
+	value,
+	icon,
+	helperText,
+	loading,
+	className,
+}: {
+	title: string;
+	value: string;
+	helperText: string;
+	loading: boolean;
+	icon: ReactNode;
+	className: string;
+}) => {
+	return (
+		<Card className={className}>
+			<CardHeader className="flex flex-row items-center justify-between pb-2">
+				<CardTitle className="text-sm font-medium">{title}</CardTitle>
+				{icon}
+			</CardHeader>
+			<CardContent>
+				<div className="text-2xl font-bold">
+					{loading && (
+						<Skeleton>
+							<span className="opacity-0">0</span>
+						</Skeleton>
+					)}
+				</div>
+				<p className="text-xs text-muted-foreground pt-1">{helperText}</p>
+			</CardContent>
+		</Card>
+	);
+};
