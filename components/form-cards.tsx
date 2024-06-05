c5040b0512fe3b8fd591daa8b008a4f7112fb990
Devi prasad <prasad20697@gmail.com>
2024-06-05 23:42:07 +0530

    form-cards component

--- components/form-cards.tsx
+++ components/form-cards.tsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React from "react";
+import { Skeleton } from "./ui/skeleton";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "./ui/card";
+import {
+	EyeOpenIcon,
+	ViewVerticalIcon,
+	MagicWandIcon,
+	DoubleArrowRightIcon,
+} from "@radix-ui/react-icons";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { GetForms } from "@/actions/form";
+import { Form } from "@prisma/client";
+import { Badge } from "./ui/badge";
 
-const FormCards = () => {
-  return (
-    <>FormCards</>
-  )
-}
+const FormCards = async () => {
+	const forms = await GetForms();
+	return (
+		<>
+			{forms.map((form) => (
+				<FormCard key={form.id} form={form} />
+			))}
+		</>
+	);
+};
 
-export default FormCards
+export default FormCards;
+
+export const FormCardSkeleton = () => {
+	return <Skeleton className="border-2 border-primary/20 h-48 w-full" />;
+};
+
+const FormCard = ({ form }: { form: Form }) => {
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle className="flex items-center gap-2 justify-between">
+					<span className="truncate font-bold">{form.name}</span>
+					{form.published && <Badge>Published</Badge>}
+					{!form.published && <Badge variant="destructive">Draft</Badge>}
+				</CardTitle>
+				<CardDescription className="flex items-center justify-between text-sm text-muted-foreground">
+					<span className="flex items-center gap-2">
+						<EyeOpenIcon className="text-muted-foreground" />
+						<span>{form.visits.toLocaleString()}</span>
+						<ViewVerticalIcon className="text-muted-foreground" />
+						<span>{form.submissions.toLocaleString()}</span>
+					</span>
+				</CardDescription>
+			</CardHeader>
+			<CardContent className="h-5 truncate text-sm text-muted-foreground">
+				{form.description || "No Description"}
+			</CardContent>
+			<CardFooter>
+				{form.published && (
+					<Button asChild className="w-full mt-2 text-md gap-2">
+						<Link href={`/forms/`}>
+							View Submission <DoubleArrowRightIcon />
+						</Link>
+					</Button>
+				)}
+				{!form.published && (
+					<Button
+						asChild
+						variant="secondary"
+						className="w-full mt-2 text-md gap-2"
+					>
+						<Link href={`/builder/`}>
+							<MagicWandIcon />
+							Edit
+						</Link>
+					</Button>
+				)}
+			</CardFooter>
+		</Card>
+	);
+};
