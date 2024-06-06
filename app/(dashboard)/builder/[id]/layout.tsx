d233a67d5442481368d48a832f501d2974363449
Devi prasad <prasad20697@gmail.com>
2024-06-06 20:03:22 +0530

    builder/[id] route

---
+++ app/(dashboard)/builder/[id]/layout.tsx
@@ -0,0 +1,9 @@
+import React, { ReactNode } from "react";
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return <div className="flex w-full flex-grow mx-auto">{children}</div>;
+}
---
+++ app/(dashboard)/builder/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BuilderPage = () => {
+  return (
+    <div>Form Builder Page</div>
+  )
+}
+
+export default BuilderPage
