7d1c34f6627c139693d54a91d693259bc1e041c6
Devi prasad <prasad20697@gmail.com>
2024-06-06 20:04:18 +0530

    ind stat card

---
+++ components/stats-card.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React, { ReactNode } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Skeleton } from "./ui/skeleton";
+
+interface StatsCardProps {
+	title: string;
+	value: string;
+	helperText: string;
+	loading: boolean;
+	icon: ReactNode;
+	className: string;
+}
+
+const StatsCard = ({
+	title,
+	value,
+	icon,
+	helperText,
+	loading,
+	className,
+}: StatsCardProps) => {
+	return (
+		<Card className={className}>
+			<CardHeader className="flex flex-row items-center justify-between pb-2">
+				<CardTitle className="text-sm font-medium">{title}</CardTitle>
+				{icon}
+			</CardHeader>
+			<CardContent>
+				<div className="text-2xl font-bold">
+					{loading && (
+						<Skeleton>
+							<span className="opacity-0">{value}</span>
+						</Skeleton>
+					)}
+				</div>
+				<p className="text-xs text-muted-foreground pt-1">{helperText}</p>
+			</CardContent>
+		</Card>
+	);
+};
+
+export default StatsCard;
