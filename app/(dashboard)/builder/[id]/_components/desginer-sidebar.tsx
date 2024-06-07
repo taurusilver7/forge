e297237010b1a6c90b8cd4edcbd374276b850abd
Devi prasad <prasad20697@gmail.com>
2024-06-07 20:44:11 +0530

    designer sidebar

---
+++ app/(dashboard)/builder/[id]/_components/desginer-sidebar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DesignerSidebar = () => {
+	
+	return (
+		<aside className="w-96 max-w-[400px] h-full m-auto rounded-xl flex flex-col flex-grow border-l-2 border-muted p-4 bg-background overflow-y-auto">
+         Elements
+		</aside>
+	);
+};
+
+export default DesignerSidebar;
