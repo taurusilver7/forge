4613a036ad067bb14923e5b7652ce9990c1d1bec
Devi prasad <prasad20697@gmail.com>
2024-06-07 08:29:49 +0530

    get forms by id server action

--- actions/form.ts
+++ actions/form.ts
@@ -84,3 +84,18 @@ export async function GetForms() {
 		},
 	});
 }
+
+export async function GetFormById(id: string) {
+	const user = await currentUser();
+
+	if (!user) {
+		throw new UserNotFoundErr();
+	}
+
+	return await prisma.form.findUnique({
+		where: {
+			userId: user.id,
+			id,
+		},
+	});
+}
