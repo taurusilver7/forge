feb4e6495eef2d407eb826f83b90097befd77ec0
Devi prasad <prasad20697@gmail.com>
2024-06-08 21:41:31 +0530

    builder designer component

--- app/(dashboard)/builder/[id]/_components/designer.tsx
+++ app/(dashboard)/builder/[id]/_components/designer.tsx
@@ -6,23 +6,32 @@ import DesignerSidebar from "./desginer-sidebar";
 import { useDroppable } from "@dnd-kit/core";
 
 const Designer = () => {
-	const droppable = {
+	const droppable = useDroppable({
 		id: "designer-drop-area",
 		data: {
 			isDesignerDropArea: true,
 		},
-	};
+	});
 	return (
-		<div className="h-full w-full flex">
-			<div className="p-4 w-full">
+		<div className="h-full w-full flex p-1">
+			<div className="p-2 w-full">
 				<div
+					ref={droppable.setNodeRef}
 					className={cn(
-						"bg-background max-w-4xl h-full rounded-xl flex flex-col flex-grow items-center justify-start flex-1 overflow-y-auto"
+						"bg-background max-w-4xl h-full rounded-xl flex flex-col flex-grow items-center justify-start flex-1 overflow-y-auto",
+						droppable.isOver && "ring-4 ring-primary/20 ring-inset"
 					)}
 				>
-					<p className="text-3xl text-muted-foreground flex flex-grow items-center font-bold">
-						Drop here
-					</p>
+					{droppable.isOver && (
+						<div className="flex flex-col w-full gap-2 p-4">
+							<div className="bg-primary/20 h-32 rounded-md"></div>
+						</div>
+					)}
+					{!droppable.isOver && (
+						<p className="text-3xl text-muted-foreground flex flex-grow items-center font-bold">
+							Drop here
+						</p>
+					)}
 				</div>
 			</div>
 
