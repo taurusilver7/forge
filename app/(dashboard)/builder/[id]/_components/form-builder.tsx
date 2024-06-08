e9e99478e2323f62b53648a83ae58cca82799b8a
Devi prasad <prasad20697@gmail.com>
2024-06-08 21:42:03 +0530

    form builder

--- app/(dashboard)/builder/[id]/_components/form-builder.tsx
+++ app/(dashboard)/builder/[id]/_components/form-builder.tsx
@@ -5,33 +5,48 @@ import SaveBtn from "./save";
 import Publish from "./publish";
 import Preview from "./preview";
 import Designer from "./designer";
+import { DndContext } from "@dnd-kit/core";
+import DragOverlayWrapper from "./drag-overlay";
+import { DoubleArrowUpIcon, PinTopIcon } from "@radix-ui/react-icons";
+import { cn } from "@/lib/utils";
 
 const FormBuilder = ({ form }: { form: Form }) => {
 	return (
-		<main className="flex flex-col w-full">
-			<nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
-				<h2 className="truncate font-medium">
-					<span className="text-muted-foreground mr-2">Form:</span>
-					{form.name}
-				</h2>
+		<DndContext>
+			<main className="flex flex-col w-full">
+				<nav className="flex group/topbar justify-between border-b-2 p-4 gap-3 items-center relative">
+					<div
+						className={cn(
+							"h-8 w-8 text-muted-foreground hover:bg-neutral-100 z-50 dark:hover:bg-neutral-700 absolute top-16 right-1/4 opacity-100 group-hover/topbar:opacity-100 transition rounded-full"
+						)}
+						role="button"
+					>
+						<DoubleArrowUpIcon className="w-6 h-6" />
+					</div>
+					<h2 className="truncate font-medium">
+						<span className="text-muted-foreground mr-2">Form:</span>
+						{form.name}
+					</h2>
 
-				<div className="flex items-center gap-2">
-					<Preview />
+					<div className="flex items-center gap-2">
+						<Preview />
 
-					{!form.published && (
-						<>
-							<SaveBtn />
-							<Publish />
-						</>
-					)}
+						{!form.published && (
+							<>
+								<SaveBtn />
+								<Publish />
+							</>
+						)}
+					</div>
+				</nav>
+
+				<div className="relative overflow-y-auto h-52 flex-grow flex w-full items-center justify-center bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
+					{/* Form Editor */}
+					<Designer />
 				</div>
-			</nav>
-
-			<div className="relative overflow-y-auto h-52 flex-grow flex w-full items-center justify-center bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
-				{/* Form Editor */}
-				<Designer />
-			</div>
-		</main>
+			</main>
+			<DragOverlayWrapper />
+		</DndContext>
 	);
 };
 
