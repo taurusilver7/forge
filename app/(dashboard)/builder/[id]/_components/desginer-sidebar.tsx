cdb1052811ddaa675ec848304666cfd55fce6796
Devi prasad <prasad20697@gmail.com>
2024-06-08 21:41:41 +0530

    designer sidebar

--- app/(dashboard)/builder/[id]/_components/desginer-sidebar.tsx
+++ app/(dashboard)/builder/[id]/_components/desginer-sidebar.tsx
@@ -1,10 +1,12 @@
+import { FormElements } from "@/components/form-elements";
+import SidebarElement from "@/components/sidebar-element";
 import React from "react";
 
 const DesignerSidebar = () => {
-	
 	return (
 		<aside className="w-96 max-w-[400px] h-full m-auto rounded-xl flex flex-col flex-grow border-l-2 border-muted p-4 bg-background overflow-y-auto">
-         Elements
+			Elements
+			<SidebarElement formElement={FormElements.TextField} />
 		</aside>
 	);
 };
