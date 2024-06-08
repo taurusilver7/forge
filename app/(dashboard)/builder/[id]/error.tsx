9b40b19f6a70f7b34f707b3277c70c0280d1dad2
Devi prasad <prasad20697@gmail.com>
2024-06-08 21:40:50 +0530

    builder error page

--- app/(dashboard)/builder/[id]/error.tsx
+++ app/(dashboard)/builder/[id]/error.tsx
@@ -1,16 +1,40 @@
 "use client";
 
-import React, { useEffect } from "react";
+import { Button } from "@/components/ui/button";
 
-function ErrorPage({ error }: { error: Error }) {
-	useEffect(() => {
-		console.log(error);
-	}, [error]);
+import Image from "next/image";
+import Link from "next/link";
+
+import React from "react";
+
+const Error = () => {
 	return (
-		<div className="flex w-full h-full flex-col items-center justify-center gap-4">
-			<h2 className="text-destructive text-4xl">Something went wrong</h2>
+		<div className="h-full w-full flex flex-col items-center justify-center space-y-4">
+			<Image
+				src="/error.png"
+				alt="empty"
+				height="400"
+				width="400"
+				className="dark:hidden"
+			/>
+			<Image
+				src="/error-dark.png"
+				alt="empty"
+				height="400"
+				width="400"
+				className="dark:block hidden"
+				priority
+			/>
+			<h2 className="text-xl font-medium text-center">
+				Oops! Looks like you&apos;re lost here!
+				<br />
+				<span className="">No worries!</span>
+			</h2>
+			<Button asChild>
+				<Link href="/">Go back</Link>
+			</Button>
 		</div>
 	);
-}
+};
 
-export default ErrorPage;
+export default Error;
