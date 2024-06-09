348ba14309d5a89b414bd7a4bf20af7a943410b7
Devi prasad <prasad20697@gmail.com>
2024-06-09 11:24:03 +0530

    custom hook

--- hooks/useDesigner.tsx
+++ hooks/useDesigner.tsx
@@ -1,2 +1,15 @@
-"use client"
+"use client";
 
+import { DesignerContext } from "@/components/context/designer-context";
+import React, { useContext } from "react";
+
+const useDesigner = () => {
+	const context = useContext(DesignerContext);
+
+	if (!context) {
+		throw new Error("useDesigner must be used within a DesignContext.");
+	}
+	return context;
+};
+
+export default useDesigner;
