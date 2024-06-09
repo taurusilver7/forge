ce71c0de6a4ecdd0eecea314362f2a6a122fcbad
Devi prasad <prasad20697@gmail.com>
2024-06-09 21:40:28 +0530

    text field extra attributes

--- components/fields/text-field.tsx
+++ components/fields/text-field.tsx
@@ -1,10 +1,26 @@
 "use client";
 
-import { ElementType, FormElement } from "../form-elements";
-import { MdTextFields } from "react-icons/md";
+import {
+	ElementType,
+	FormElement,
+	FormElementInstance,
+} from "../form-elements";
 import { TextIcon } from "@radix-ui/react-icons";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
 
 const type: ElementType = "TextField";
+
+const extraAttributes = {
+	label: "Text Field",
+	helperText: "Helper Text",
+	required: false,
+	placeholder: "Value here...",
+};
+
+type CustomInstance = FormElementInstance & {
+	extraAttributes: typeof extraAttributes;
+};
 
 export const TextFieldFormElement: FormElement = {
 	type,
@@ -12,18 +28,34 @@ export const TextFieldFormElement: FormElement = {
 	construct: (id: string) => ({
 		id,
 		type,
-		extraAttributes: {
-			label: "Text Field",
-			helperText: "Helper Text",
-			required: false,
-			placeholder: "Value here...",
-		},
+		extraAttributes,
 	}),
 	designerBtnElement: {
 		icon: TextIcon,
 		label: "Text Field",
 	},
-	designerComponent: () => <div className="text-white">Designer Component</div>,
+	designerComponent: DesignerComponent,
 	formComponent: () => <div>Form Component</div>,
 	propertiesComponent: () => <div>Properties Component</div>,
 };
+
+function DesignerComponent({
+	elementInstance,
+}: {
+	elementInstance: FormElementInstance;
+}) {
+	const element = elementInstance as CustomInstance;
+	const { label, placeholder, required, helperText } = element.extraAttributes;
+	return (
+		<div className="flex flex-col gap-2 w-full text-white">
+			<Label>
+				{label}
+				{required && "*"}
+			</Label>
+			<Input readOnly disabled placeholder={placeholder} />
+			{helperText && (
+				<p className="text-muted-foreground text-xs">{helperText}</p>
+			)}
+		</div>
+	);
+}
