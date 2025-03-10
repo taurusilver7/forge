0c7d198acb44cd1a590d3152e172dbeb3f428a03
Taurus Silver <taurusilver7@gmail.com>
2025-03-10 18:15:41 +0530

    buider error page

--- app/(dashboard)/builder/[id]/error.tsx
+++ app/(dashboard)/builder/[id]/error.tsx
@@ -1,40 +1,42 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import React, { useEffect } from "react";
 
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
-import React from "react";
-
-const Error = () => {
-	return (
-		<div className="h-full w-full flex flex-col items-center justify-center space-y-4">
-			<Image
-				src="/error.png"
-				alt="empty"
-				height="400"
-				width="400"
-				className="dark:hidden"
-			/>
-			<Image
-				src="/error-dark.png"
-				alt="empty"
-				height="400"
-				width="400"
-				className="dark:block hidden"
-				priority
-			/>
-			<h2 className="text-xl font-medium text-center">
-				Oops! Looks like you&apos;re lost here!
-				<br />
-				<span className="">No worries!</span>
-			</h2>
-			<Button asChild>
-				<Link href="/">Go back</Link>
-			</Button>
-		</div>
-	);
+const Error = ({ error }: { error: Error }) => {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+  return (
+    <div className="h-full w-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/error.png"
+        alt="empty"
+        height="400"
+        width="400"
+        className="dark:hidden"
+      />
+      <Image
+        src="/error-dark.png"
+        alt="empty"
+        height="400"
+        width="400"
+        className="dark:block hidden"
+        priority
+      />
+      <h2 className="text-xl font-medium text-center">
+        Oops! Looks like you&apos;re lost here!
+        <br />
+        <span className="">No worries!</span>
+      </h2>
+      <Button asChild>
+        <Link href="/">Go back</Link>
+      </Button>
+    </div>
+  );
 };
 
 export default Error;
