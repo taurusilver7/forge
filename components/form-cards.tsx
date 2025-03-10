f4c45baca7c28c8226a35958466c9d56bb00aaf0
Taurus Silver <taurusilver7@gmail.com>
2025-03-10 18:16:21 +0530

    form card

--- components/form-cards.tsx
+++ components/form-cards.tsx
@@ -43,12 +43,12 @@ const FormCard = ({ form }: { form: Form }) => {
 		<Card>
 			<CardHeader>
 				<CardTitle className="flex items-center gap-2 justify-between">
-					<span className="truncate font-bold">{form.name}</span>
+					<span className="truncate font-bold uppercase">{form.name}</span>
 					{form.published && <Badge>Published</Badge>}
 					{!form.published && <Badge variant="destructive">Draft</Badge>}
 				</CardTitle>
 				<CardDescription className="flex items-center justify-between text-xs text-muted-foreground">
-					{formatDistance(form.createdAt, new Date())}
+					{formatDistance(form.createdAt, new Date(), {addSuffix: true})}
 					{form.published && (
 						<span className="flex items-center gap-2">
 							<EyeOpenIcon className="text-muted-foreground" />
@@ -73,7 +73,7 @@ const FormCard = ({ form }: { form: Form }) => {
 				{!form.published && (
 					<Button
 						asChild
-						variant="secondary"
+						variant={"secondary"}
 						className="w-full mt-2 text-md gap-2"
 					>
 						<Link href={`/builder/${form.id}`}>
