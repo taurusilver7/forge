7a52a9d8f8ccadf87b77374be2ed9ca61a27f26f
Taurus Silver <taurusilver7@gmail.com>
2025-03-12 21:20:50 +0530

    designer context

--- components/context/designer-context.tsx
+++ components/context/designer-context.tsx
@@ -4,29 +4,34 @@ import { createContext, ReactNode, useState } from "react";
 import { FormElementInstance } from "../form-elements";
 
 type DesignerContextType = {
-	elements: FormElementInstance[];
-	addElement: (index: number, element: FormElementInstance) => void;
+  elements: FormElementInstance[];
+  addElement: (index: number, element: FormElementInstance) => void;
+  removeElement: (id: string) => void;
 };
 
 export const DesignerContext = createContext<DesignerContextType | null>(null);
 
 export default function DesignerContextProvider({
-	children,
+  children,
 }: {
-	children: ReactNode;
+  children: ReactNode;
 }) {
-	const [elements, setElements] = useState<FormElementInstance[]>([]);
+  const [elements, setElements] = useState<FormElementInstance[]>([]);
 
-	const addElement = (index: number, element: FormElementInstance) => {
-		setElements((prev) => {
-			const newElements = [...prev];
-			newElements.splice(index, 0, element);
-			return newElements;
-		});
-	};
-	return (
-		<DesignerContext.Provider value={{ elements, addElement }}>
-			{children}
-		</DesignerContext.Provider>
-	);
+  const addElement = (index: number, element: FormElementInstance) => {
+    setElements((prev) => {
+      const newElements = [...prev];
+      newElements.splice(index, 0, element);
+      return newElements;
+    });
+  };
+
+  const removeElement = (id: string) => {
+    setElements((prev) => prev.filter((el) => el.id !== id));
+  };
+  return (
+    <DesignerContext.Provider value={{ elements, addElement, removeElement }}>
+      {children}
+    </DesignerContext.Provider>
+  );
 }
