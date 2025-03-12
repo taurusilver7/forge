1018b7e853fb9372dc068254899a7e6eaf30fee9
Taurus Silver <taurusilver7@gmail.com>
2025-03-12 21:20:28 +0530

    logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 import React from "react";
 
 const Logo = () => {
-	return (
-		<Link
-			href={"/"}
-			className="font-bold text-3xl bg-gradient-to-r from-indigo-500 to-cyan-600 text-transparent bg-clip-text hover:cursor-pointer"
-		>
-			Forge
-		</Link>
-	);
+  return (
+    <Link
+      href={"/"}
+      className="font-bold text-3xl bg-gradient-to-r from-indigo-500 to-cyan-600 text-transparent bg-clip-text hover:cursor-pointer"
+    >
+      Forge
+    </Link>
+  );
 };
 
 export default Logo;
