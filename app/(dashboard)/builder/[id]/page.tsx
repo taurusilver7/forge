4936d201d898f1e98c1695c15ae078885c06895f
Taurus Silver <taurusilver7@gmail.com>
2025-04-09 11:22:41 +0530

    builder page

--- app/(dashboard)/builder/[id]/page.tsx
+++ app/(dashboard)/builder/[id]/page.tsx
@@ -2,10 +2,9 @@ import React from "react";
 import { GetFormById } from "@/actions/form";
 import FormBuilder from "./_components/form-builder";
 
-const BuilderPage = async ({ params }: { params: { id: string } }) => {
+const BuilderPage = async ({ params }: { params: { id: number } }) => {
   const { id } = params;
   const form = await GetFormById(Number(id));
-
   if (!form) {
     throw new Error("Form not found!");
   }
