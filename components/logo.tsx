4c825db41a8183952681991ead43de15797737e9
Devi prasad <prasad20697@gmail.com>
2025-05-02 10:57:55 +0530

    logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,15 +1,19 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Logo = () => {
-  return (
-    <Link
-      href={"/"}
-      className="font-bold text-3xl bg-gradient-to-r from-indigo-500 to-cyan-600 text-transparent bg-clip-text hover:cursor-pointer"
-    >
-      Forge
-    </Link>
-  );
+	return (
+		<Link
+			href={"/"}
+			className="no-underline flex gap-2  hover:cursor-pointer"
+		>
+			<Image src="/logo.svg" alt="logo" width={40} height={40} />
+			<span className="text-3xl font-bold bg-gradient-to-r from-purple-400 via-violet-700 to-violet-500 text-transparent bg-clip-text">
+				Forge
+			</span>
+		</Link>
+	);
 };
 
 export default Logo;
