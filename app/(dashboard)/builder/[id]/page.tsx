8c2dae62020bfb91462584a040daece90557c1f8
Devi prasad <prasad20697@gmail.com>
2025-05-06 16:43:42 +0530

    builder page

--- app/(dashboard)/builder/[id]/page.tsx
+++ app/(dashboard)/builder/[id]/page.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import { GetFormById } from "@/actions/form";
 import FormBuilder from "./_components/form-builder";
 
-const BuilderPage = async ({ params }: { params: { id: number } }) => {
-  const { id } = params;
-  const form = await GetFormById(Number(id));
-  if (!form) {
-    throw new Error("Form not found!");
-  }
+const BuilderPage = async ({ params }: { params: { id: string } }) => {
+	const { id } = params;
+	const form = await GetFormById(id);
+	if (!form) {
+		throw new Error("Form not found!");
+	}
 
-  // console.log(form);
+	// console.log(form);
 
-  return <FormBuilder form={form} />;
+	return <FormBuilder form={form} />;
 };
 
 export default BuilderPage;
