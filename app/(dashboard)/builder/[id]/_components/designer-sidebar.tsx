81561cb7b32c2e8f2130a9d8c50d26fa2eeaf346
Devi prasad <prasad20697@gmail.com>
2025-05-06 16:43:52 +0530

    sidebar elements sidebar

--- app/(dashboard)/builder/[id]/_components/designer-sidebar.tsx
+++ app/(dashboard)/builder/[id]/_components/designer-sidebar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const DesignerSidebar = () => {
 	return (
-		<aside className="w-96 max-w-[400px] h-full m-auto rounded-xl flex flex-col flex-grow border-l-2 border-muted p-4 bg-background overflow-y-auto">
+		<aside className="w-96 max-w-96 h-full m-auto rounded-xl flex flex-col flex-grow border-l-2 border-muted p-4 bg-background overflow-y-auto">
 			Form Elements
 			<SidebarElement formElement={FormElements.TextField} />
 		</aside>
