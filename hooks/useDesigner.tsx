399c02aaafe1392f0ee2bd151a3354f2ab4c804d
Devi prasad <prasad20697@gmail.com>
2025-05-06 16:48:28 +0530

    context api useDesigner

--- hooks/useDesigner.tsx
+++ hooks/useDesigner.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { DesignerContext } from "@/components/context/designer-context";
-import React, { useContext } from "react";
+import { useContext } from "react";
 
 const useDesigner = () => {
 	const context = useContext(DesignerContext);
 
 	if (!context) {
-		throw new Error("useDesigner must be used within a DesignContext.");
+		throw new Error("useDesigner must be used within a DesignerContext.");
 	}
 	return context;
 };
