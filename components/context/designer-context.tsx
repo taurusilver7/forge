8ccbfcc4e8e3b3a828350e7bbeff83b71c14dc72
Devi prasad <prasad20697@gmail.com>
2025-05-06 16:47:32 +0530

    designer context

--- components/context/designer-context.tsx
+++ components/context/designer-context.tsx
@@ -1,37 +1,47 @@
 "use client";
 
-import { createContext, ReactNode, useState } from "react";
+import { createContext, useState } from "react";
 import { FormElementInstance } from "../form-elements";
 
 type DesignerContextType = {
-  elements: FormElementInstance[];
-  addElement: (index: number, element: FormElementInstance) => void;
-  removeElement: (id: string) => void;
+	elements: FormElementInstance[];
+	removeElement: (id: string) => void;
+	addElement: (index: number, element: FormElementInstance) => void;
 };
 
 export const DesignerContext = createContext<DesignerContextType | null>(null);
 
 export default function DesignerContextProvider({
-  children,
+	children,
 }: {
-  children: ReactNode;
+	children: React.ReactNode;
 }) {
-  const [elements, setElements] = useState<FormElementInstance[]>([]);
+	const [elements, setElements] = useState<FormElementInstance[]>([]);
 
-  const addElement = (index: number, element: FormElementInstance) => {
-    setElements((prev) => {
-      const newElements = [...prev];
-      newElements.splice(index, 0, element);
-      return newElements;
-    });
-  };
+	const addElement = (index: number, element: FormElementInstance) => {
+		setElements((prev) => {
+			const newElements = [...prev];
+			newElements.splice(index, 0, element);
+			return newElements;
+		});
+	};
 
-  const removeElement = (id: string) => {
-    setElements((prev) => prev.filter((el) => el.id !== id));
-  };
-  return (
-    <DesignerContext.Provider value={{ elements, addElement, removeElement }}>
-      {children}
-    </DesignerContext.Provider>
-  );
+	const removeElement = (id: string) => {
+		console.log("Removing element", id);
+		setElements((prev) => {
+			const newElements = [...prev];
+			const index = newElements.findIndex((e) => e.id === id);
+
+			if (index !== -1) {
+				newElements.splice(index, 1);
+			}
+
+			return newElements;
+		});
+	};
+	return (
+		<DesignerContext.Provider value={{ elements, addElement, removeElement }}>
+			{children}
+		</DesignerContext.Provider>
+	);
 }
