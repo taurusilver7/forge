f9572771569d78f76d3ca5e2379f7447541156d5
Devi prasad <prasad20697@gmail.com>
2025-05-06 16:43:27 +0530

    toaster to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
+
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme";
-import { Toaster } from "@/components/ui/toaster";
 import DesignerContextProvider from "@/components/context/designer-context";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,7 +30,7 @@ export default function RootLayout({
 }) {
 	return (
 		<ClerkProvider>
-			<html lang="en" suppressHydrationWarning>
+			<html lang="en">
 				<body className={inter.className}>
 					<DesignerContextProvider>
 						<ThemeProvider
@@ -38,6 +40,7 @@ export default function RootLayout({
 							disableTransitionOnChange
 						>
 							{children}
+							<Analytics />
 							<Toaster />
 						</ThemeProvider>
 					</DesignerContextProvider>
