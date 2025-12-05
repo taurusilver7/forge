3673d1ead4751916878ae4779fed1ce3ee839248
Devi prasad <prasad20697@gmail.com>
2025-12-05 22:11:33 +0530

    designer context

--- components/context/designer-context.tsx
+++ components/context/designer-context.tsx
@@ -1,12 +1,18 @@
 "use client";
 
-import { createContext, useState } from "react";
+import { createContext, useState, Dispatch, SetStateAction } from "react";
 import { FormElementInstance } from "../form-elements";
 
 type DesignerContextType = {
 	elements: FormElementInstance[];
 	removeElement: (id: string) => void;
+	setElements: Dispatch<SetStateAction<FormElementInstance[]>>;
 	addElement: (index: number, element: FormElementInstance) => void;
+
+	selectedElement: FormElementInstance | null;
+	setSelectedElement: Dispatch<SetStateAction<FormElementInstance | null>>;
+
+	updateElement: (id: string, element: FormElementInstance) => void;
 };
 
 export const DesignerContext = createContext<DesignerContextType | null>(null);
@@ -17,6 +23,8 @@ export default function DesignerContextProvider({
 	children: React.ReactNode;
 }) {
 	const [elements, setElements] = useState<FormElementInstance[]>([]);
+	const [selectedElement, setSelectedElement] =
+		useState<FormElementInstance | null>(null);
 
 	const addElement = (index: number, element: FormElementInstance) => {
 		setElements((prev) => {
@@ -28,19 +36,47 @@ export default function DesignerContextProvider({
 
 	const removeElement = (id: string) => {
 		console.log("Removing element", id);
+		setElements(
+			(prev) => prev.filter((element) => element.id !== id)
+
+			/*
+			Alternate method to filter out hte selected element to remove.
+				{
+				const newElements = [...prev];
+				const index = newElements.findIndex((e) => e.id === id);
+
+				if (index !== -1) {
+					newElements.splice(index, 1);
+				}
+
+				return newElements;
+			}
+			*/
+		);
+	};
+
+	const updateElement = (id: string, element: FormElementInstance) => {
 		setElements((prev) => {
 			const newElements = [...prev];
-			const index = newElements.findIndex((e) => e.id === id);
-
-			if (index !== -1) {
-				newElements.splice(index, 1);
-			}
-
+			const index = newElements.findIndex((el) => el.id === id);
+			newElements[index] = element;
 			return newElements;
 		});
 	};
+
 	return (
-		<DesignerContext.Provider value={{ elements, addElement, removeElement }}>
+		<DesignerContext.Provider
+			value={{
+				elements,
+				addElement,
+				removeElement,
+				setElements,
+
+				selectedElement,
+				setSelectedElement,
+				updateElement,
+			}}
+		>
 			{children}
 		</DesignerContext.Provider>
 	);
