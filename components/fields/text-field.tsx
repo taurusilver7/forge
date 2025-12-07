42ffccad14ab0fe3341cb4c9e26c502d65118bae
Devi prasad <prasad20697@gmail.com>
2025-12-07 22:26:19 +0530

    a text field

--- components/fields/text-field.tsx
+++ components/fields/text-field.tsx
@@ -4,10 +4,19 @@ import {
 	ElementType,
 	FormElement,
 	FormElementInstance,
+	SubmitFunction,
 } from "@/components/form-elements";
 import { TextIcon } from "@radix-ui/react-icons";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
+import { z } from "zod";
+import { TextFieldSchema } from "@/lib/schema";
+import useDesigner from "@/hooks/useDesigner";
+import { Form } from "../ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const type: ElementType = "TextField";
 
@@ -35,8 +44,9 @@ export const TextFieldFormElement: FormElement = {
 		label: "Text Field",
 	},
 	designerComponent: DesignerComponent,
-	formComponent: () => <div>Form Component</div>,
-	propertiesComponent: () => <div>Properties Component</div>,
+	formComponent: FormComponent,
+	propertiesComponent: PropertiesComponent,
+
 	validate: (
 		formElement: FormElementInstance,
 		currentValue: string
@@ -67,5 +77,107 @@ function DesignerComponent({
 				<p className="text-muted-foreground text-xs">{helperText}</p>
 			)}
 		</div>
+	);
+}
+
+interface FormComponentProps {
+	elementInstance: FormElementInstance;
+	submitValue?: SubmitFunction;
+	isInvalid?: boolean;
+	defaultValue?: string;
+}
+
+function FormComponent({
+	elementInstance,
+	submitValue,
+	isInvalid,
+	defaultValue,
+}: FormComponentProps) {
+	const element = elementInstance as CustomInstance;
+	const [value, setValue] = useState<string>(defaultValue || "");
+	const [error, setError] = useState(false);
+
+	useEffect(() => {
+		setError(isInvalid === true);
+	}, [isInvalid]);
+
+	const { label, required, helperText, placeholder } = element.extraAttributes;
+	return (
+		<div className="flex flex-col gap-2 w-full">
+			<Label className={cn(error && "text-red-500")}>
+				{label}
+				{required && ""}
+			</Label>
+			<Input
+				className={cn(error && "border-red-500")}
+				placeholder={placeholder}
+				onChange={(e) => setValue(e.target.value)}
+				onBlur={(e) => {
+					if (!submitValue) return;
+					const valid = TextFieldFormElement.validate(
+						element,
+						e.target.value
+					);
+					setError(!valid);
+					if (!valid) return;
+					submitValue(element.id, e.target.value);
+				}}
+				value={value}
+			/>
+			{helperText && (
+				<p
+					className={cn(
+						"text-muted-foreground text-sm",
+						error && "text-red-500"
+					)}
+				>
+					{helperText}
+				</p>
+			)}
+		</div>
+	);
+}
+
+type textFieldSchemaType = z.infer<typeof TextFieldSchema>;
+function PropertiesComponent({
+	elementInstance,
+}: {
+	elementInstance: FormElementInstance;
+}) {
+	const element = elementInstance as CustomInstance;
+
+	const { updateElement } = useDesigner();
+	const form = useForm<textFieldSchemaType>({
+		resolver: zodResolver(TextFieldSchema),
+		mode: "onBlur",
+		defaultValues: {
+			label: element.extraAttributes.label,
+			helperText: element.extraAttributes.helperText,
+			required: element.extraAttributes.required,
+			placeholder: element.extraAttributes.placeholder,
+		},
+	});
+
+	useEffect(() => {
+		form.reset(element.extraAttributes);
+	}, [element, form]);
+
+	function applyChanges(values: textFieldSchemaType) {
+		const { helperText, label, placeholder, required } = values;
+		updateElement(element.id, {
+			...element,
+			extraAttributes: {
+				label,
+				helperText,
+				placeholder,
+				required,
+			},
+		});
+	}
+
+	return (
+		<Form {...form}>
+			<form className="space-y-3"></form>
+		</Form>
 	);
 }
