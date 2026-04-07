4c560f1edebdae856e734e1fed79a5c7b6f98d0d
Devi prasad <prasad20697@gmail.com>
2026-04-07 18:09:01 +0530

    html render suppress hydration errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
 }) {
 	return (
 		<ClerkProvider>
-			<html lang="en">
+			<html lang="en" suppressHydrationWarning>
 				<body className={inter.className}>
 					<DesignerContextProvider>
 						<ThemeProvider
