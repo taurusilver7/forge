04b7a9b8d5340f704e5b12b4b8c7b18c75dc93a7
Devi prasad <prasad20697@gmail.com>
2026-07-24 16:29:59 +0530

    form loading

--- app/(dashboard)/form/[id]/loading.tsx
+++ app/(dashboard)/form/[id]/loading.tsx
@@ -1,14 +1,1 @@
-import React from "react";
-import { Loader2 } from "lucide-react";
-
-
-
-function Loading() {
-   return (
-      <div className="flex items-center justify-center w-full h-full">
-         <Loader2 className="animate-spin h-12 w-12" />
-      </div>
-   );
-}
-
-export default Loading;
+export { default } from "@/components/loading"
